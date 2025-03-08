e870a80354de6e0c2be99e5974a422a4974326f2
shuvadeepmondal <sumonmondal19413@outlook.com>
2025-03-08 23:57:23 +0530

    Responsive Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./components/home/Home";
-// import Navbar from "./components/utilis/Navbar";
-// import Qrcodegen from "./components/qrcodegen/Qrcodegen";
-
-// import ParticleBackground from "./components/ui/Particlebackground";
-import SolarSystem from "./components/ui/SolarSystem";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Navbar from "./components/utilis/Navbar";
+import Qrcodegen from "./components/qrcodegen/Qrcodegen";
 
 
 const App: React.FC = () => {
   return (
       <div className="min-h-screen">
-        <SolarSystem/>
-        {/* <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/qrgeneration" element={<Qrcodegen />} />
-        </Routes> */}
+        </Routes>
       </div>
   );
 
